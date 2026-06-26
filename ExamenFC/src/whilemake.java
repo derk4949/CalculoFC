a5fd0a73f7ae06b98f49bf6c1b71c85447e278b7
Derk4949 <xcal4949@hotmail.com>
2026-06-25 19:34:03 -0500

    MEJORADO CON CONTROL DE INTENTOS

--- ExamenFC/src/whilemake.java
+++ ExamenFC/src/whilemake.java
@@ -6,22 +6,29 @@ public class whilemake {
         // menor y si indica un numero negativo volver a pedirle
         Scanner lector = new Scanner(System.in);
         int edad;
+        int control = 3;
         System.out.println("ingrese su edad");
         edad = lector.nextInt();
-        while (edad < 0){
+        while (edad < 0 && control > 0){
             System.out.println("Valor incorrecto, Vuelve a intentarlo");
             edad = lector.nextInt();
+            control = control-1;
+            System.out.println("TE QUEDA = "+ control+ " INTENTOS");
         }
-        System.out.println("edad ingresada es "+edad);
+        if (edad < 0){
+            System.out.println("SE TE ACABARON LOS INTENTOS");
+        }
+        else {
+            System.out.println("edad ingresada es "+edad);
 
-        if (edad >= 18 && edad <= 100) {
-            System.out.println("edad = " + edad + " , es valido para el ingreso");
-        } else if (edad<= 17){
+            if (edad >= 18 && edad <= 100) {
+                System.out.println("edad = " + edad + " , es valido para el ingreso");
+            } else if (edad<= 17){
                 System.out.println("largate mocoso");
             } else {
-            System.out.println("a chistoso te crees");
+                System.out.println("a chistoso te crees");
+            }
         }
-
 
 
     }
